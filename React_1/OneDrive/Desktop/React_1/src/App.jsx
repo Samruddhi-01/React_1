473fe546e9b4c1e32b4102b2b3f888d76e5be866
Samruddhi-01 <159429733+Samruddhi-01@users.noreply.github.com>
2024-11-28 19:57:13 +0530

    Add files via upload
    Landing page is added

---
+++ React_1/OneDrive/Desktop/React_1/src/App.jsx
@@ -0,0 +1,11 @@
+import'./App.css';
+import HeroSection from './componets/Hero';
+import Navigation from './componets/navigation';
+const App = () =>{
+    return <div>
+       <Navigation/>
+        <HeroSection/>
+    </div>;
+
+};
+export default App;
---
+++ src/componets/Hero.jsx
@@ -0,0 +1,25 @@
+const HeroSection = () =>{
+    return <main className="Hero container">
+        <div className="content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <div 
+        className="herobtn">
+            <button>shop now</button>
+            <button className="secondary-btn">Category</button>
+        </div>
+        <div
+            className ="Shopping"
+        >
+        <div className="brandicon">
+            <img src="/images/amezone.png" alt="logos"/>
+        </div>
+        </div>
+        </div>
+        <div className="images">
+        <img src="/images/shoe_image.png" alt="shoe"/>
+        </div>
+
+    </main>
+};
+export default HeroSection;
